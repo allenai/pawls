0cae02978d80313dccdfff7d6dd2c277fe3c03f1
Mark Neumann <markng@allenai.org>
2020-08-17 17:30:16 -0700

    start of api

--- api/main.py
+++ api/main.py
@@ -3,12 +3,20 @@ import logging
 import os
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from pdfstructure.client.v1.api.default_api import DefaultApi
+from pdfstructure.client.v1.configuration import Configuration
+from pdfstructure.client.v1.api_client import ApiClient
+from pdfstructure.client.v1 import models as model
+
 
 from app.utils import StackdriverJsonFormatter
 
+IN_PRODUCTION = os.getenv("IN_PRODUCTION", "dev") 
+
 handlers = None
-if os.getenv("IN_PRODUCTION", "dev") == "prod":
+if  IN_PRODUCTION == "prod":
     json_handler = logging.StreamHandler()
     json_handler.setFormatter(StackdriverJsonFormatter())
     handlers = [json_handler]
@@ -20,6 +28,10 @@ logging.basicConfig(
 logger = logging.getLogger("uvicorn")
 
 
+
+pdf_structure_client = DefaultApi(ApiClient(Configuration(host=f"http://pdf-structure-{IN_PRODUCTION}.us-west-2.elasticbeanstalk.com")))
+
+
 app = FastAPI(root_path="/api")
 
 @app.get("/", status_code=204)
@@ -29,3 +41,31 @@ def read_root():
     root URL, so it can tell the service is ready for requests.
     """
     return {}
+
+
+@app.get("/tokens/{sha}")
+def get_tokens(sha: str, source: Optional[str] = "all"):
+    """
+
+    sha: str
+        PDF sha to retrieve from the PDF structure service.
+    source: str (default = "all")
+        The comma separated string of annotation sources to fetch. This allows fetching of specific annotations.
+
+    """
+    annotations = pdf_structure_client.get_annotations(sha, token_sources=source)
+
+    tokens = annotations.get("tokens", None)
+    if tokens is None:
+        raise HttpException(status_code=404, detail=f"No PDF found for {sha}, or invalid source ({source})")
+
+    return tokens["tokens"]["sources"].to_dict()
+
+@app.get("/elements/{sha}")
+def get_text_elements(sha: str, source= "all"):
+    pass
+
+@app.get("/region/{sha}")
+def get_region(sha: str, source= "all"):
+    pass
+
