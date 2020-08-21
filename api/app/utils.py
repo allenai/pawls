ca16680fa8e02046032cfacd5ca6f5c14478fa1f
Mark Neumann <mark.neumann.1992@gmail.com>
2020-08-21 11:04:22 -0700

    download pdfs (#11)
    * download pdfs
    
    * notes, keys in compose.yaml
    
    * wip serve pdfs
    
    * move mounted volume, git keep it
    
    Co-authored-by: Mark Neumann <markng@allenai.org>

--- api/app/utils.py
+++ api/app/utils.py
@@ -1,3 +1,8 @@
+from typing import List, Dict
+import os
+
+import boto3
+import botocore
 from pythonjsonlogger import jsonlogger
 
 class StackdriverJsonFormatter(jsonlogger.JsonFormatter):
@@ -15,3 +20,53 @@ class StackdriverJsonFormatter(jsonlogger.JsonFormatter):
         super(StackdriverJsonFormatter, self).add_fields(log_record, record,
             message_dict)
         log_record['severity'] = record.levelname
+
+# settings for S3 buckets
+S3_BUCKET_PDFS = {
+    'default': 'ai2-s2-pdfs',
+    'private': 'ai2-s2-pdfs-private'
+}
+
+def bulk_fetch_pdfs_for_s2_ids(s2_ids: List[str], target_dir: str) -> Dict[str, List[str]]:
+    """
+
+    s2_ids: List[str]
+        A list of s2 pdf shas to download.
+
+    target_dir: str,
+        The directory to download them to.
+
+    Note:
+    User is responsible for figuring out whether the PDF already exists before
+    calling this function.  By default, will perform overwriting.
+
+    Returns
+    A dict containing "error" and "not_found" keys, listing pdf shas that were either
+    not found or errored when fetching.
+    """
+
+    os.makedirs(target_dir, exist_ok=True)
+    s3 = boto3.resource('s3')
+    default_bucket = s3.Bucket(S3_BUCKET_PDFS['default'])
+    private_bucket = s3.Bucket(S3_BUCKET_PDFS['private'])
+
+    not_found = []
+    error = []
+    for s2_id in s2_ids:
+        try:
+            default_bucket.download_file(os.path.join(s2_id[:4], f'{s2_id[4:]}.pdf'),
+                                         os.path.join(target_dir, f'{s2_id}.pdf'))
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                try:
+                    private_bucket.download_file(os.path.join(s2_id[:4], f'{s2_id[4:]}.pdf'),
+                                                 os.path.join(target_dir, f'{s2_id}.pdf'))
+                except botocore.exceptions.ClientError as e:
+                    if e.response['Error']['Code'] == "404":
+                        not_found.append(s2_id)
+                    else:
+                        error.append(s2_id)
+            else:
+                error.append(s2_id)
+        
+    return {"error": error, "not_found": not_found}
