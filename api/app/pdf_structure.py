1fddb47c06c1f5e53d9fda2b8924c4495af7027f
Mark Neumann <markng@allenai.org>
2020-08-28 09:20:09 -0700

    formatting

--- api/app/pdf_structure.py
+++ api/app/pdf_structure.py
@@ -1,4 +1,3 @@
-
 from typing import List
 import os
 import six
@@ -10,18 +9,20 @@ from pdfstructure.client.v1.configuration import Configuration
 from pdfstructure.client.v1.api_client import ApiClient
 from pdfstructure.client.v1 import models as model
 
-IN_PRODUCTION = os.getenv("IN_PRODUCTION", "dev") 
+IN_PRODUCTION = os.getenv("IN_PRODUCTION", "dev")
 
 # HACK: The OpenAPI generator used to generate the Python client
 # for the pdf structure service has a bug which means it cannot
 # deserialize itself properly if it has nested container types.
 # This fixes it, so we are monkeypatching the one class we need.
 # See: https://github.com/allenai/s2-pdf-structure-service/issues/11
+
+
 def to_dict_patch(self):
     """Returns the model properties as a dict"""
 
     def handle_list(item):
-        
+
         ret = []
         for i in item:
             if hasattr(i, "to_dict"):
@@ -44,24 +45,29 @@ def to_dict_patch(self):
     for attr, _ in six.iteritems(self.openapi_types):
         value = getattr(self, attr)
         if isinstance(value, list):
-            result[attr] = list(map(
-                lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
-                value
-            ))
+            result[attr] = list(
+                map(lambda x: x.to_dict() if hasattr(x, "to_dict") else x, value)
+            )
         elif hasattr(value, "to_dict"):
             result[attr] = value.to_dict()
         elif isinstance(value, dict):
-            result[attr] = dict(map(handle_dict,
-                value.items()
-            ))
+            result[attr] = dict(map(handle_dict, value.items()))
         else:
             result[attr] = value
 
     return result
 
+
 model.TextElementTypes.to_dict = to_dict_patch
 
-pdf_structure_client = DefaultApi(ApiClient(Configuration(host=f"http://pdf-structure-{IN_PRODUCTION}.us-west-2.elasticbeanstalk.com")))
+pdf_structure_client = DefaultApi(
+    ApiClient(
+        Configuration(
+            host=f"http://pdf-structure-{IN_PRODUCTION}.us-west-2.elasticbeanstalk.com"
+        )
+    )
+)
+
 
 # TODO(Mark): Wrap this in a LRU cache.
 def _get_annotations(sha: str) -> model.PdfAnnotations:
@@ -72,15 +78,17 @@ def get_annotations(
     sha: str,
     token_sources: List[str] = None,
     text_element_sources: List[str] = None,
-    region_sources: List[str] = None
-    ):
+    region_sources: List[str] = None,
+):
 
     full_annotations = _get_annotations(sha)
     response = {}
 
     if token_sources:
         if full_annotations.tokens is None:
-            raise HTTPException(status_code=404, detail=f"Non-existent token annotations for sha {sha}")
+            raise HTTPException(
+                status_code=404, detail=f"Non-existent token annotations for sha {sha}"
+            )
 
         # Short circuit logic for all annotations.
         if "all" in token_sources:
@@ -91,13 +99,19 @@ def get_annotations(
             for source in token_sources:
                 data = full_annotations.tokens.sources.get(source, None)
                 if data is None:
-                    raise HTTPException(status_code=404, detail=f"Non-existent token source f{source} for sha {sha}")
+                    raise HTTPException(
+                        status_code=404,
+                        detail=f"Non-existent token source f{source} for sha {sha}",
+                    )
 
                 response["tokens"]["sources"][source] = data.to_dict()
 
     if text_element_sources:
         if full_annotations.text_elements is None:
-            raise HTTPException(status_code=404, detail=f"Non-existent text element annotations for sha {sha}")
+            raise HTTPException(
+                status_code=404,
+                detail=f"Non-existent text element annotations for sha {sha}",
+            )
 
         # Short circuit logic for all annotations.
         if "all" in text_element_sources:
@@ -108,13 +122,18 @@ def get_annotations(
             for source in text_element_sources:
                 data = full_annotations.text_elements.sources.get(source, None)
                 if data is None:
-                    raise HTTPException(status_code=404, detail=f"Non-existent text element source f{source} for sha {sha}")
+                    raise HTTPException(
+                        status_code=404,
+                        detail=f"Non-existent text element source f{source} for sha {sha}",
+                    )
 
                 response["text_elements"]["sources"][source] = data.to_dict()
 
     if region_sources:
         if full_annotations.regions is None:
-            raise HTTPException(status_code=404, detail=f"Non-existent region annotations for sha {sha}")
+            raise HTTPException(
+                status_code=404, detail=f"Non-existent region annotations for sha {sha}"
+            )
 
         # Short circuit logic for all annotations.
         if "all" in region_sources:
@@ -125,7 +144,10 @@ def get_annotations(
             for source in region_sources:
                 data = full_annotations.regions.sources.get(source, None)
                 if data is None:
-                    raise HTTPException(status_code=404, detail=f"Non-existent region source f{source} for sha {sha}")
+                    raise HTTPException(
+                        status_code=404,
+                        detail=f"Non-existent region source f{source} for sha {sha}",
+                    )
 
                 response["regions"]["sources"][source] = data.to_dict()
 
--- api/test/test_utils.py
+++ api/test/test_utils.py
@@ -5,7 +5,6 @@ import tempfile
 from app.utils import bulk_fetch_pdfs_for_s2_ids
 
 
-
 class TestUtils(unittest.TestCase):
 
     def test_bulk_fetch_pdfs(self):
@@ -17,7 +16,7 @@ class TestUtils(unittest.TestCase):
 
     def test_bulk_fetch_nonexistent_pdfs(self):
 
-        shas = [ "abc", "34f25a8704614163c4095b3ee2fc969b60de4698"]
+        shas = ["abc", "34f25a8704614163c4095b3ee2fc969b60de4698"]
         with tempfile.TemporaryDirectory() as tempdir:
             errors = bulk_fetch_pdfs_for_s2_ids(shas, tempdir)
             assert os.path.exists(os.path.join(tempdir, shas[1] + ".pdf"))
