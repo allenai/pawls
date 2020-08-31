5328a75e09503e7f151472c1b9b365664882513b
Mark Neumann <markng@allenai.org>
2020-08-31 13:52:18 -0700

    on the fly ES client, use get not search

--- api/app/metadata.py
+++ api/app/metadata.py
@@ -10,17 +10,10 @@ ES_CLIENTS = {"dev": None, "prod": None}
 
 
 def _get_es_client(use_prod=False):
-
-    prod_or_dev = "prod" if use_prod else "dev"
     host = ELASTIC_SEARCH_URL_PORT_PROD if use_prod else ELASTIC_SEARCH_URL_PORT_DEV
-
-    if ES_CLIENTS[prod_or_dev] is not None:
-        return ES_CLIENTS[prod_or_dev]
-    else:
-        ES_CLIENTS[prod_or_dev] = elasticsearch.Elasticsearch(
-            hosts=[{"host": host, "port": 9200}], timeout=20
+    return elasticsearch.Elasticsearch(
+            hosts=[{"host": host, "port": 9200}], timeout=5
         )
-        return ES_CLIENTS[prod_or_dev]
 
 
 class PaperMetadata(NamedTuple):
@@ -30,16 +23,6 @@ class PaperMetadata(NamedTuple):
     year: int
     cited_by: int
     authors: List[str]
-
-    def to_dict(self):
-        return {
-            "sha": self.sha,
-            "title": self.title,
-            "venue": self.venue,
-            "year": self.year,
-            "cited_by": self.cited_by,
-            "authors": self.authors,
-        }
 
 
 def get_paper_metadata(
@@ -57,16 +40,15 @@ def get_paper_metadata(
     PaperMetadata if the paper is found, otherwise None.
     """
     ES = _get_es_client(use_prod=use_prod)
-    result = ES.search(
-        index="paper_v3",
-        body={
-            "query": {"term": {"id": paper_sha}},
-            "_source": ["title", "venue", "year", "numCitedBy", "authors"],
-        },
-    )
+    try:
+        result = ES.get(
+            index="paper_v3",
+            doc_type="paper",
+            id=paper_sha,
+            _source=["title", "venue", "year", "numCitedBy", "authors"],
+        )
 
-    if len(result["hits"]["hits"]) > 0:
-        hit = result["hits"]["hits"][0]["_source"]
+        hit = result["_source"]
         return PaperMetadata(
             sha=paper_sha,
             title=hit["title"],
@@ -75,5 +57,5 @@ def get_paper_metadata(
             cited_by=int(hit["numCitedBy"]),
             authors=[a["name"] for a in hit["authors"]],
         )
-    else:
+    except elasticsearch.exceptions.NotFoundError:
         return None
