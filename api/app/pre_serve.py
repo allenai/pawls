5b9314fa447ee63578c36cb96883920426da8822
Mark Neumann <markng@allenai.org>
2020-09-03 09:51:01 -0700

    comment

--- api/app/pre_serve.py
+++ api/app/pre_serve.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Dict
 
 import json
 import glob
@@ -13,7 +13,23 @@ IN_PRODUCTION = os.getenv("IN_PRODUCTION", "dev")
 
 
 class Configuration(NamedTuple):
+    """
+    General configuration for the annotation tool.
 
+    output_directory: str, required.
+        The directory where the pdfs, metadata and
+        annotation output will be stored.
+    labels: List[str], required.
+        The labels in use for annotation.
+    pdfs: List[str], required.
+        The pdfs that you want to annotate. This can be updated,
+        and new pdfs will be _added_ to the annotation set. Removing
+        pdfs from this list once the app has been started with them
+        will not remove them from the annotation.
+    preprocessors: List[str], optional (default = None)
+        Optional pre-processing steps to apply to pdfs. Currently,
+        the only supported option is "grobid".
+    """
     output_directory: str
     labels: List[str]
     pdfs: List[str]
@@ -21,9 +37,18 @@ class Configuration(NamedTuple):
 
 
 class Annotators(NamedTuple):
+    """
+    Configuration for Annotators.
 
+    annotators: List[str], required.
+        A list of gmail emails for annotators.
+        Currently these must be ai2 emails.
+    allocations: Dict[str, List[str]], required.
+        A mapping from annotator emails to lists of pdf shas
+        to which they are assigned.
+    """
     annotators: List[str]
-    allocations: List[str]
+    allocations: Dict[str, List[str]]
 
 
 def load_configuration(filepath: str) -> Configuration:
@@ -68,6 +93,8 @@ def maybe_download_pdfs(configuration: Configuration):
 
         json.dump(metadata._asdict(), open(metadata_path, "w+"))
 
+        # Populate the pdf-structure-service with grobid
+        # annotations for the pdf.
         if "grobid" in configuration.preprocessors:
             pdf_path = _per_dir_pdf_download(configuration.output_directory, sha)
             process_grobid(sha, pdf_path, env=IN_PRODUCTION)
