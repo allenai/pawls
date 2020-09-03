a78614fd42ef79b97b09f781bf33aee7da617d5d
Mark Neumann <mark.neumann.1992@gmail.com>
2020-09-03 09:55:30 -0700

    Integration (#26)
    * checkpoint, this may not be the way
    
    * way easier SQS way
    
    * remove alternative way
    
    * Revert "remove alternative way"
    
    This reverts commit d8df2bc24823dd6eb3f1a9ff438d650cf3cbb3af.
    
    * local version again
    
    * comment
    
    * lint
    
    Co-authored-by: Mark Neumann <markng@allenai.org>

--- api/app/pre_serve.py
+++ api/app/pre_serve.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Dict
 
 import json
 import glob
@@ -7,18 +7,50 @@ import os
 from app.metadata import get_paper_metadata
 from app.utils import bulk_fetch_pdfs_for_s2_ids
 
+from app.structure.grobid import process_grobid
+
+IN_PRODUCTION = os.getenv("IN_PRODUCTION", "dev")
+
 
 class Configuration(NamedTuple):
+    """
+    General configuration for the annotation tool.
+
+    output_directory: str, required.
+        The directory where the pdfs, metadata and
+        annotation output will be stored.
+    labels: List[str], required.
+        The labels in use for annotation.
+    pdfs: List[str], required.
+        The pdfs that you want to annotate. This can be updated,
+        and new pdfs will be _added_ to the annotation set. Removing
+        pdfs from this list once the app has been started with them
+        will not remove them from the annotation.
+    preprocessors: List[str], optional (default = None)
+        Optional pre-processing steps to apply to pdfs. Currently,
+        the only supported option is "grobid".
+    """
 
     output_directory: str
     labels: List[str]
     pdfs: List[str]
+    preprocessors: List[str] = None
 
 
 class Annotators(NamedTuple):
+    """
+    Configuration for Annotators.
+
+    annotators: List[str], required.
+        A list of gmail emails for annotators.
+        Currently these must be ai2 emails.
+    allocations: Dict[str, List[str]], required.
+        A mapping from annotator emails to lists of pdf shas
+        to which they are assigned.
+    """
 
     annotators: List[str]
-    allocations: List[str]
+    allocations: Dict[str, List[str]]
 
 
 def load_configuration(filepath: str) -> Configuration:
@@ -62,3 +94,9 @@ def maybe_download_pdfs(configuration: Configuration):
             metadata = get_paper_metadata(sha, use_prod=True)
 
         json.dump(metadata._asdict(), open(metadata_path, "w+"))
+
+        # Populate the pdf-structure-service with grobid
+        # annotations for the pdf.
+        if "grobid" in configuration.preprocessors:
+            pdf_path = _per_dir_pdf_download(configuration.output_directory, sha)
+            process_grobid(sha, pdf_path, env=IN_PRODUCTION)
