b5efff3c4bbec8489ad848670973f7e4ba209854
Mark Neumann <markng@allenai.org>
2020-09-09 10:38:10 -0700

    remove es from requirements

--- api/app/metadata.py
+++ api/app/metadata.py
@@ -1,17 +1,8 @@
 from typing import NamedTuple, List, Optional
-import elasticsearch
+import requests
 
 
-# URLs for elastic search queries
-ELASTIC_SEARCH_URL_PORT_DEV = "es5.cf.development.s2.dev.ai2"
-ELASTIC_SEARCH_URL_PORT_PROD = "es5.cf.production.s2.prod.ai2.in"
-
-ES_CLIENTS = {"dev": None, "prod": None}
-
-
-def _get_es_client(use_prod=False):
-    host = ELASTIC_SEARCH_URL_PORT_PROD if use_prod else ELASTIC_SEARCH_URL_PORT_DEV
-    return elasticsearch.Elasticsearch(hosts=[{"host": host, "port": 9200}], timeout=5)
+S2_API = "https://www.semanticscholar.org/api/1/paper/"
 
 
 class PaperMetadata(NamedTuple):
@@ -23,37 +14,29 @@ class PaperMetadata(NamedTuple):
     authors: List[str]
 
 
-def get_paper_metadata(
-    paper_sha: str, use_prod: bool = False
-) -> Optional[PaperMetadata]:
+def get_paper_metadata(paper_sha: str) -> Optional[PaperMetadata]:
     """
-    Fetch a small metadata blob for a paper from Elasticsearch.
+    Fetch a small metadata blob from S2.
 
     paper_sha: str, required
         The paper id to search for.
-    use_prod: bool, (default = False)
-        Whether to use the production elastic search cluster.
 
     returns:
-    PaperMetadata if the paper is found, otherwise None.
+        PaperMetadata if the paper is found, otherwise None.
     """
-    ES = _get_es_client(use_prod=use_prod)
-    try:
-        result = ES.get(
-            index="paper_v3",
-            doc_type="paper",
-            id=paper_sha,
-            _source=["title", "venue", "year", "numCitedBy", "authors"],
-        )
 
-        hit = result["_source"]
+    response = requests.get(S2_API + paper_sha)
+    if response.ok:
+        data = response.json()
+
         return PaperMetadata(
             sha=paper_sha,
-            title=hit["title"],
-            venue=hit["venue"],
-            year=int(hit["year"]),
-            cited_by=int(hit["numCitedBy"]),
-            authors=[a["name"] for a in hit["authors"]],
+            title=data["paper"]["title"]["text"],
+            venue=data["paper"]["venue"]["text"],
+            year=int(data["paper"]["year"]["text"]),
+            cited_by=int(data["paper"]["citationStats"]["numCitations"]),
+            authors=[author[0]["name"] for author in data["paper"]["authors"]]
         )
-    except elasticsearch.exceptions.NotFoundError:
+
+    else:
         return None
