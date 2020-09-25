8b111b4b2ca91b7de13b39920a9036107167c9b3
Mark Neumann <markng@allenai.org>
2020-09-25 10:14:04 -0700

    add my budget logo

--- ui/src/components/sidebar/Header.tsx
+++ ui/src/components/sidebar/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Logos } from '@allenai/varnish';
 
+import pawlsLogo from "./pawlsLogo.png";
+import styled from 'styled-components';
+
 const { AI2Logo } = Logos;
 
 export const Header = () => {
@@ -8,8 +11,14 @@ export const Header = () => {
     return (
         <>
             <AI2Logo color="white" size="micro"/>
-            <h2>Pawls</h2>
+            <Logo src={pawlsLogo}/>
         </>
     );
 
 };
+
+const Logo = styled.img`
+    margin: 20px 4px 10px 0px;
+    padding: 4px;
+    max-width:100%;
+`
