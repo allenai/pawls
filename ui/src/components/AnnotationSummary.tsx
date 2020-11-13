ac647974e17f59f2db9a6a7cdcbf215f8458e7af
Mark Neumann <mark.neumann.1992@gmail.com>
2020-11-13 09:34:44 -0800

    tags in sidebar annotation view (#68)
    * tags in sidebar annotation view
    
    * fix css, thanks @codeviking
    
    Co-authored-by: Mark Neumann <markng@allenai.org>

--- ui/src/components/AnnotationSummary.tsx
+++ ui/src/components/AnnotationSummary.tsx
@@ -12,26 +12,49 @@ interface AnnotationSummaryProps {
 export const AnnotationSummary = ({annotation, pageInfo}: AnnotationSummaryProps) => {
 
     const text = annotation.tokens === null ? "Freeform" : 
-            annotation.tokens.map(t => pageInfo.tokens[t.tokenIndex].text).join(" ")
+            annotation.tokens
+            .map(t => pageInfo.tokens[t.tokenIndex].text)
+            .join(" ")
 
     return (
-        <div>
-            {text}
-            <div>
-                <Details>
-                    <Tag color={annotation.label.color}>
-                        {annotation.label.text}
-                    </Tag>
-                    <Tag>
-                        Page {pageInfo.page.pageNumber}
-                    </Tag>
-                </Details>
-            </div>
-        </div>
+        <Hoverable>
+            <Overflow>
+                {text}
+            </Overflow>
+                <SmallTag color={annotation.label.color}>
+                    {annotation.label.text}
+                </SmallTag>
+                <SmallTag color="grey">
+                    Page {pageInfo.page.pageNumber}
+                </SmallTag>
+        </Hoverable>
     );
 
 }
 
-const Details = styled.span`
-    font-size: 10px
+export const Hoverable = styled.div(({ theme }) => `
+    background: ${theme.color.N9};
+    padding: 4px 0;
+    border-radius: 2px;
+    display: grid;
+    grid-template-columns: minmax(0, 1fr) min-content min-content;
+
+    :hover {
+        background: ${theme.color.N8}
+    }
+`);
+
+
+const SmallTag = styled(Tag)`
+    font-size: 10px;
+    padding: 2px 2px;
+    border-radius: 4px;
+    color: black;
+    line-height: 1;
+`
+const Overflow = styled.span`
+    font-size: 12px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;   
 `
